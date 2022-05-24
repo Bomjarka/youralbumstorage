de25e42e0e768567eec5ded0876b384e86edcb21
a.chirkin <a.chirkin@robo.finance>
2022-05-24 21:34:46 +0700

    change maintenance.blade.php

--- resources/views/errors/maintenance.blade.php
+++ resources/views/errors/maintenance.blade.php
@@ -1,18 +1,45 @@
 <!doctype html>
 <title>Site Maintenance</title>
 <style>
-    body { text-align: center; padding: 150px; }
-    h1 { font-size: 50px; }
-    body { font: 20px Helvetica, sans-serif; color: #333; }
-    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-    a { color: #dc8100; text-decoration: none; }
-    a:hover { color: #333; text-decoration: none; }
-</style>
+    body {
+        text-align: center;
+        padding: 150px;
+    }
 
+    h1 {
+        font-size: 50px;
+    }
+
+    body {
+        font: 20px Helvetica, sans-serif;
+        color: #333;
+        background-image: url('/images/background.jpg')
+    }
+
+    article {
+        display: block;
+        text-align: left;
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    a {
+        color: #dc8100;
+        text-decoration: none;
+    }
+
+    a:hover {
+        color: #333;
+        text-decoration: none;
+    }
+</style>
+<body>
 <article>
     <h1>{{ trans('maintenance-page.header') }}</h1>
     <div>
         <p>{{ trans('maintenance-page.message') }}</p>
-        <p>&mdash; {{ config('app.name') }} </p>
+        <p>&mdash; <a href="mailto:{{ config('mail.from.address', 'YourAlbumStorage') }}">{{ config('app.name') }} </p>
     </div>
 </article>
+</body>
+
