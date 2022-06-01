27792b66cd9fec08c44940b2bc926c43939fbd1e
a.chirkin <a.chirkin@robo.finance>
2022-06-01 18:44:41 +0700

    YAS-7 change translations for delete photo modal

--- resources/views/components/photos/delete-photo.blade.php
+++ resources/views/components/photos/delete-photo.blade.php
@@ -52,12 +52,12 @@
                             @method('post')
                             <button type="submit"
                                     class="deleteModalButton w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm">
-                                {{ trans('delete-photo-form.cancel-button') }}
+                                {{ trans('delete-photo-form.delete-button') }}
                             </button>
                         </form>
                         <button @click="open = false"
                                 class="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm">
-                            {{ trans('delete-photo-form.delete-button') }}
+                            {{ trans('delete-photo-form.cancel-button') }}
                         </button>
                     </div>
                 </div>
